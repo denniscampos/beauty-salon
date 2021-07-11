852a6697651180c9224780ccda93f749dbb2ca49
Dennis Campos <dennisjr89@gmail.com>
2021-07-11 16:17:33 -0400

    added media queries for hero section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.querySelector(".toggle-bar");
+const navBarLinks = document.querySelector(".navbar-links");
 
+toggleBtn.addEventListener("click", () => {
+  navBarLinks.classList.toggle("active");
+});
